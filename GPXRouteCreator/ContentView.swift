2a44e8a5ed10427bbfd3359afa1f4dcb7c31464f
Dominik Hauser <dom@dasdom.de>
2020-02-07 10:35:54 +0100

    Initial Commit

---
+++ GPXRouteCreator/ContentView.swift
@@ -0,0 +1,17 @@
+//  Created by dasdom on 07.02.20.
+//  Copyright © 2020 dasdom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
